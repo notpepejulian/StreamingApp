77f4279b98c3feab231b1aa194a32a21412f9e57
NachoooSanchez <nacho_sanchez@epamneoris.com>
2025-08-05 18:59:52 +0200

    Revert "PostgreSQL & Backend logic."
    This reverts commit 01dff7836ea86dc2fd6523f81282520cc5efb769.

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'secret-key-default')
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@localhost:5432/{os.getenv('DB_NAME')}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- backend/routes/content.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from models import Content, db
-
-content_bp = Blueprint('content', __name__)
-
-@content_bp.route('/movies', methods=['GET'])
-def get_movies():
-    movies = Content.query.join(Movie).all()
-    return jsonify([{'id': m.id, 'title': m.title} for m in movies])
